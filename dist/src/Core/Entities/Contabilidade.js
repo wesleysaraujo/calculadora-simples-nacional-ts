e4f06452c482fad05511ec875109243a4a0a4312
misteregis <misteregis@gmail.com>
2023-05-07 17:12:44 -0300

    Removed content from dist folder

--- dist/src/Core/Entities/Contabilidade.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Contabilidade = void 0;
-class Contabilidade {
-    constructor() {
-    }
-}
-exports.Contabilidade = Contabilidade;
